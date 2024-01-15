cf98986a5d5c39dfc2eae24861b61cf6f9840bb4
jonathas <jonathas09@gmail.com>
2024-01-15 20:51:00 -0300

    diminuição de mensagem para verificar interfaçe mobile

--- src/chat_component_message.js
+++ src/chat_component_message.js
@@ -6,7 +6,7 @@ const ChatComponentMessage = () => {
     <div>
         <div className='message_user'>
             <div className='chat_message_user'>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
             </div>
             <div className='hours_message_user'>
                 <p className='hours'>05:20</p>  
@@ -18,7 +18,7 @@ const ChatComponentMessage = () => {
                 <p className='hours'>05:20</p>  
             </div>
             <div className='chat_message_assistant'>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
             </div>
             
         </div>    
