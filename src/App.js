0743e72d6785e43040064255071241ee406c5f0e
jonathas <jonathas09@gmail.com>
2024-01-25 19:19:42 -0300

    ajuste css, criado variavel de recebimento de textos e mapeamento

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import user_image from './147144.png';
 import './App.css';
-import ChatComponentMessage from './chat_component_message';
+
+import ChatComponentMessage from './chatComponentMessage';
+import { useTypingEffect } from './hooks/typingeffect';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const text = useTypingEffect("I'm your travel assistant", 20);
+  const text2 = useTypingEffect("mr. flight", 20);
+
+  const [name, setName] = useState('user');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        name: 'mr. flight',
+        text: 'I am your travel assistant, ask me anything, I will try to help you...',
+        sender: 'assistant',
+        timestamp: '01:00'
+      },
+      {
+        name: 'mr. flight',
+        text: 'I am your travel assistant',
+        sender: 'user',
+        timestamp: '02:00'
+      },
+      {
+        name: 'mr. flight',
+        text: 'I am your travel assistant',
+        sender: 'assistant',
+        timestamp: '02:20'
+      },
+      {
+        name: 'mr. flight',
+        text: 'I am your travel assistant',
+        sender: 'user',
+        timestamp: '10:00'
+      }
+    ]);
+  }, []);
+
   return (
-    <div className="App">
-
-
-      <body>
+    
+      <body className="App">
         <div className='chat'>
           <div className='chat_header'>
             <div className='header_photo'>
               <img src={user_image} className="photo_header_img" alt="logo" />
             </div>
             <div className='header_info'>
-              <p className='ai_name'>Assistant Name</p>
-              <p className='ai_status'>your travel assistant</p>
+              <p className='ai_name'>{text2}</p>
+              <p className='ai_status'>{text}</p>
             </div>
           </div>
-          <ChatComponentMessage />
-
+          <div className='chat_messages'>
+            {messages.map((message, index) => (
+              <ChatComponentMessage
+                key={index}
+                sender={message.sender}
+                message={message.text}
+                time={message.timestamp}
+              />
+            ))}
+          </div>
         </div>
       </body>
-    </div>
+    
   );
 }
 
---
+++ src/chatComponentMessage.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+
+const ChatComponentMessage = ({ message, time, sender }) => { // Destructure the props
+    if (sender === 'user') {
+        return (
+                <div className='message_user'>
+                    <div className='chat_message_user'>
+                        <p>{message}</p>
+                    </div>
+                    <div className='hours_message_user'>
+                        <p className='hours'>{time}</p>  
+                    </div>
+                </div>
+        )        
+    } else {
+        return (    
+            <div className='message_assistant'>
+                <div className='hours_message_user'>
+                    <p className='hours'>{time}</p>  
+                </div>
+                <div className='chat_message_assistant'>
+                    <p>{message}</p>
+                </div>
+                
+            </div>   
+        )
+    }
+};
+
+export default ChatComponentMessage;
